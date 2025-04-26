71da3c7175ac24277d6e173d01dbae4885219c20
abdulrahmanibrahimdawood <abdoibrahim9111@gmail.com>
2025-04-26 15:26:39 +0300

    book entity

---
+++ lib/Features/home/domain/entities/book_entity.dart
@@ -0,0 +1,14 @@
+class BookEntity {
+  final String title;
+  final String authorName;
+  final String image;
+  final num price;
+  final num rating;
+
+  BookEntity(
+      {required this.title,
+      required this.authorName,
+      required this.image,
+      required this.price,
+      required this.rating});
+}
