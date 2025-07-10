300593cb392f33cc4607ef5104bec179a316db6b
abdulrahmanibrahimdawood <abdoibrahim9111@gmail.com>
2025-07-10 18:10:53 +0300

    pagination

--- lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
+++ lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -4,12 +4,14 @@ import 'package:clean_arc_bookly_app/core/errors/failure.dart';
 import 'package:clean_arc_bookly_app/core/use_cases/use_case.dart';
 import 'package:dartz/dartz.dart';
 
-class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>, NoParam> {
+class FetchFeaturedBooksUseCase extends UseCase<List<BookEntity>, int> {
   final HomeRepo homeRepo;
   FetchFeaturedBooksUseCase(this.homeRepo);
 
   @override
-  Future<Either<Failure, List<BookEntity>>> call([NoParam? param]) async {
-    return await homeRepo.fetchFeaturedBooks();
+  Future<Either<Failure, List<BookEntity>>> call([int pageNumber = 0]) async {
+    return await homeRepo.fetchFeaturedBooks(
+      pageNumber: pageNumber,
+    );
   }
 }
