1c0beec4ca5f805341667c9b8753625453208a1e
Abdulrahman Ibrahim <abdoibrahim9111@gmail.com>
2025-08-25 15:57:40 +0300

    Featured Books ListView LoadingIndicator

--- lib/Features/home/presentation/views/widgets/featured_books_list_view_bloc_consumer.dart
+++ lib/Features/home/presentation/views/widgets/featured_books_list_view_bloc_consumer.dart
@@ -1,5 +1,6 @@
 import 'package:clean_arc_bookly_app/Features/home/domain/entities/book_entity.dart';
 import 'package:clean_arc_bookly_app/Features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
+import 'package:clean_arc_bookly_app/Features/home/presentation/views/widgets/featured_books_list_view_loading%20indicator.dart';
 import 'package:clean_arc_bookly_app/Features/home/presentation/views/widgets/featured_list_view.dart';
 import 'package:clean_arc_bookly_app/core/utils/funcations/build_error_sankbar.dart';
 import 'package:flutter/material.dart';
@@ -45,9 +46,7 @@ class _FeaturedBooksListViewBlocConsumerState
             ),
           );
         } else {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
+          return FeaturedBooksListViewLoadingIndicator();
         }
       },
     );
